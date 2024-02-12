007e9523c880717eda5c784223b34dffe637280f
rlongdon <rglongdon@gmail.com>
2024-02-12 10:28:19 +0000

    work on templates

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.Home.as_view(),name='homepage' ),
-    path('<slug:slug>/',views.TournamentView.as_view(),name='tournament'),
-    path('<slug:slug>/<slug:holiday>',views.RoundsView.as_view(),name='rounds'),
+    path('<slug:tournament>/',views.TournamentView.as_view(),name='tournament'),
+    path('<slug:tournament>/<slug:holiday>',views.RoundsView.as_view(),name='rounds'),
+    path('<slug:tournament>/<slug:holiday>/<round>',views.ScoresView.as_view(),name='scores'),
 
 ]
--- tournaments/views.py
+++ tournaments/views.py
@@ -19,24 +19,31 @@ class TournamentView(View):
 
     template_name = 'tournaments/tournament.html'
     
-    def get(self,request,slug):
-        selected_tournament = Tournament.objects.filter(slug=slug).get()
+    def get(self,request,tournament):
+        selected_tournament = Tournament.objects.filter(slug=tournament).get()
         holidays = Holiday.objects.filter(tournament=selected_tournament)
         
-        return render(request,self.template_name,{'data':holidays,'slug':slug})
+        return render(request,self.template_name,{'data':holidays,'tournament':tournament,'nest':{'layer':8}})
     
 class RoundsView(View):
     
     template_name = "tournaments/rounds.html"
     
-    def get(self,request,slug,holiday):
+    def get(self,request,tournament,holiday):
+        selected_tournament = Tournament.objects.filter(slug=tournament).get()
+        selected_holiday = Holiday.objects.filter(slug=holiday,tournament=selected_tournament)
+        holiday_filter = selected_holiday.get()
+        rounds = GolfRound.objects.filter(holiday=holiday_filter)
+        for row in rounds.values():
+            score = Score.objects.filter(golf_round_id=row['round_number'])
+            print(score)
+        return render(request,self.template_name,{'holidays':selected_holiday,'rounds':rounds,'tournament':tournament})
+    
+class ScoresView(View):
+    template_name = 'tournaments/scores.html'
+
+    def get(self,request,slug,holiday,round):
         selected_tournament = Tournament.objects.filter(slug=slug).get()
         selected_holiday = Holiday.objects.filter(slug=holiday,tournament=selected_tournament)
         holiday_filter = selected_holiday.get()
         rounds = GolfRound.objects.filter(holiday=holiday_filter)
-        # score = Score.objects.all().values()
-        # print(score)
-        for row in rounds.values():
-            score = Score.objects.filter(golf_round_id=row['round_number'])
-            print(score)
-        return render(request,self.template_name,{'holidays':selected_holiday,'rounds':rounds})
