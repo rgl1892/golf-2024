bc34802a0517af1880ac50c2a74b752017f7c28a
Rich Longdon <rglongdon@gmail.com>
2024-02-21 15:38:15 +0000

    nice summing added

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Tournament,Holiday,GolfRound,Score, Player,Hole,Handicap,Course
+from .models import Tournament, Holiday, GolfRound, Score, Player, Hole, Handicap, Course
 from django.views.generic import View
 from django.http import HttpResponse
 import math
@@ -13,105 +13,115 @@ class Home(View):
 
     model = Tournament
     template_name = 'tournaments/home.html'
-    
-    def get(self,request):
+
+    def get(self, request):
         tournaments = Tournament.objects.all()
-        return render(request,self.template_name,{'tournaments':tournaments})
-        
-    
+        return render(request, self.template_name, {'tournaments': tournaments})
+
+
 class TournamentView(View):
-    
+
     """Lists all the holidays within a tournament"""
 
     template_name = 'tournaments/tournament.html'
-    
-    def get(self,request,tournament):
+
+    def get(self, request, tournament):
         selected_tournament = Tournament.objects.filter(slug=tournament).get()
         holidays = Holiday.objects.filter(tournament=selected_tournament)
-        
-        
-        
-        return render(request,self.template_name,{'holidays':holidays,'tournament':tournament,'selected_tournament':selected_tournament})
-    
+
+        return render(request, self.template_name, {'holidays': holidays, 'tournament': tournament, 'selected_tournament': selected_tournament})
+
+
 class RoundsView(View):
-    
+
     """Lists all the rounds from a holiday"""
 
     template_name = "tournaments/rounds.html"
-    
-    def get(self,request,tournament,holiday):
+
+    def get(self, request, tournament, holiday):
         selected_tournament = Tournament.objects.filter(slug=tournament).get()
-        selected_holiday = Holiday.objects.filter(slug=holiday,tournament=selected_tournament)
+        selected_holiday = Holiday.objects.filter(
+            slug=holiday, tournament=selected_tournament)
         holiday_filter = selected_holiday.get()
         rounds = GolfRound.objects.filter(holiday=holiday_filter)
-        
-        return render(request,self.template_name,{'holiday':holiday_filter,'rounds':rounds,'tournament':tournament})
-    
+
+        return render(request, self.template_name, {'holiday': holiday_filter, 'rounds': rounds, 'tournament': tournament})
+
+
 class ScoresView(View):
     template_name = 'tournaments/scores.html'
     stableford_lookup = {
-                        '2':0,
-                        '1':1,
-                        '0':2,
-                        '-1':3,
-                        '-2':4,
-                        '-3':5,
-                        '-4':6,
-                        '-5':7,
-                        '-6':8,
-                        '-7':9
-                        }
-       
+        '2': 0,
+        '1': 1,
+        '0': 2,
+        '-1': 3,
+        '-2': 4,
+        '-3': 5,
+        '-4': 6,
+        '-5': 7,
+        '-6': 8,
+        '-7': 9
+    }
 
-    def get(self,request,tournament,holiday,selected_round):
-        
+    def get(self, request, tournament, holiday, selected_round):
+
         selected_tournament = Tournament.objects.filter(slug=tournament).get()
-        holiday_filter = Holiday.objects.filter(slug=holiday,tournament=selected_tournament).get()
-        selected_round = GolfRound.objects.filter(round_number=selected_round,holiday=holiday_filter).get()
+        holiday_filter = Holiday.objects.filter(
+            slug=holiday, tournament=selected_tournament).get()
+        selected_round = GolfRound.objects.filter(
+            round_number=selected_round, holiday=holiday_filter).get()
         scores = Score.objects.filter(golf_round=selected_round)
-        players = scores.order_by('player__first_name').values('player_id').distinct()
-        
-        
+        players = scores.order_by('player__first_name').values(
+            'player_id').distinct()
 
-        selected_course = Course.objects.filter(hole=scores.values()[0]['hole_id']).values()[0]
+        selected_course = Course.objects.filter(
+            hole=scores.values()[0]['hole_id']).values()[0]
         slope_rating = selected_course['slope_rating']
         course_rating = selected_course['course_rating']
-        
+
         handicaps = []
         for player in players:
             handicap_index = Handicap.objects.filter(id=scores.filter(player=player['player_id'],
                                                                       hole=scores.values()[0]['hole_id']).values('handicap_id')[0]['handicap_id']).values()[0]['handicap_index']
-            playing_handicap = round((slope_rating/113)*float(handicap_index)*0.95)
-            player_name = Player.objects.filter(id=player['player_id']).values('first_name')[0]['first_name']
+            playing_handicap = round(
+                (slope_rating/113)*float(handicap_index)*0.95)
+            player_name = Player.objects.filter(
+                id=player['player_id']).values('first_name')[0]['first_name']
 
-            handicaps.append([playing_handicap,player_name])
+            handicaps.append([playing_handicap, player_name, sum(
+                [score.strokes for score in scores.filter(player=player['player_id'])])])
 
-        hole_numbers = [x for x in range(1,19)]
-        player_scores = [scores.filter(player=players[x]['player_id']) for x in range(len(players))]
-            
+        hole_numbers = [x for x in range(1, 19)]
+        player_scores = [scores.filter(
+            player=players[x]['player_id']) for x in range(len(players))]
+
         context = {
-            'holiday':holiday_filter,
-            'selected_round':selected_round,
-            'tournament':tournament,
-            'scores':scores,
-            "players":players,
+            'holiday': holiday_filter,
+            'selected_round': selected_round,
+            'tournament': tournament,
+            'scores': scores,
+            "players": players,
             "hole_numbers": hole_numbers,
-            "player_scores":player_scores,
-            "handicaps":handicaps
+            "player_scores": player_scores,
+            "handicaps": handicaps
         }
-        
-        
-        return render(request,self.template_name,context)
-    
-    def post(self,request,tournament,holiday,selected_round):
+
+        return render(request, self.template_name, context)
+
+    def post(self, request, tournament, holiday, selected_round):
         selected_tournament = Tournament.objects.filter(slug=tournament).get()
-        holiday_filter = Holiday.objects.filter(slug=holiday,tournament=selected_tournament).get()
-        selected_round = GolfRound.objects.filter(round_number=selected_round,holiday=holiday_filter).get()
+        holiday_filter = Holiday.objects.filter(
+            slug=holiday, tournament=selected_tournament).get()
+        selected_round = GolfRound.objects.filter(
+            round_number=selected_round, holiday=holiday_filter).get()
         scores = Score.objects.filter(golf_round=selected_round)
-        players = scores.order_by('player__first_name').values('player_id').distinct()
-        hole_numbers = [x for x in range(1,19)]
-        player_scores = [scores.filter(player=players[x]['player_id']) for x in range(len(players))]
-        course = Course.objects.filter(id=Hole.objects.filter(id=request.POST['hole']).values()[0]['course_id'])
+        players = scores.order_by('player__first_name').values(
+            'player_id').distinct()
+        hole_numbers = [x for x in range(1, 19)]
+        player_scores = [scores.filter(
+            player=players[x]['player_id']) for x in range(len(players))]
+        course = Course.objects.filter(id=Hole.objects.filter(
+            id=request.POST['hole']).values()[0]['course_id'])
 
         # Get data on the course to calutale stableford points
         slope_rating = course.values()[0]['slope_rating']
@@ -121,15 +131,18 @@ class ScoresView(View):
         handicaps = []
 
         for player in players:
-            scores.filter(player=player['player_id'],hole=request.POST['hole']).update(strokes=request.POST[f"{player['player_id']}"])
+            scores.filter(player=player['player_id'], hole=request.POST['hole']).update(
+                strokes=request.POST[f"{player['player_id']}"])
             handicap_index = Handicap.objects.filter(id=scores.filter(player=player['player_id'],
                                                                       hole=request.POST['hole']).values('handicap_id')[0]['handicap_id']).values()[0]['handicap_index']
-            
-            playing_handicap = round((slope_rating/113)*float(handicap_index)*0.95)
-            
+
+            playing_handicap = round(
+                (slope_rating/113)*float(handicap_index)*0.95)
+
             take_off_score = math.floor(playing_handicap/18)
             si_change = playing_handicap % 18
-            shots = scores.filter(player=player['player_id'],hole=request.POST['hole']).values('strokes')[0]['strokes']
+            shots = scores.filter(player=player['player_id'], hole=request.POST['hole']).values(
+                'strokes')[0]['strokes']
             stroke_index = hole_played['stroke_index']
             par = hole_played['par']
 
@@ -143,51 +156,54 @@ class ScoresView(View):
             except:
                 points = 0
 
-            scores.filter(player=player['player_id'],hole=request.POST['hole']).update(stableford_score=points)
-            player_name = Player.objects.filter(id=player['player_id']).values('first_name')[0]['first_name']
+            scores.filter(player=player['player_id'], hole=request.POST['hole']).update(
+                stableford_score=points)
+            player_name = Player.objects.filter(
+                id=player['player_id']).values('first_name')[0]['first_name']
 
-            handicaps.append([playing_handicap,player_name])
-
+            handicaps.append([playing_handicap, player_name])
 
         context = {
-            'holiday':holiday_filter,
-            'selected_round':selected_round,
-            'tournament':tournament,
-            'scores':scores,
-            "players":players,
+            'holiday': holiday_filter,
+            'selected_round': selected_round,
+            'tournament': tournament,
+            'scores': scores,
+            "players": players,
             "hole_numbers": hole_numbers,
-            "player_scores":player_scores,
-            'handicaps':handicaps
+            "player_scores": player_scores,
+            'handicaps': handicaps
         }
-        return render(request,self.template_name,context)
+        return render(request, self.template_name, context)
+
 
 class EditScoresView(View):
     template_name = 'tournaments/edit_scores.html'
 
-    def get(self,request,tournament,holiday,selected_round,hole):
+    def get(self, request, tournament, holiday, selected_round, hole):
 
         selected_tournament = Tournament.objects.filter(slug=tournament).get()
-        holiday_filter = Holiday.objects.filter(slug=holiday,tournament=selected_tournament).get()
-        selected_round = GolfRound.objects.filter(round_number=selected_round,holiday=holiday_filter).get()
+        holiday_filter = Holiday.objects.filter(
+            slug=holiday, tournament=selected_tournament).get()
+        selected_round = GolfRound.objects.filter(
+            round_number=selected_round, holiday=holiday_filter).get()
         scores = Score.objects.filter(golf_round=selected_round)
-        players = scores.order_by('player__first_name').values('player_id').distinct()
-        
-        hole_numbers = [x for x in range(1,19)]
-        player_scores = [scores.filter(player=players[x]['player_id']) for x in range(len(players))]
+        players = scores.order_by('player__first_name').values(
+            'player_id').distinct()
+
+        hole_numbers = [x for x in range(1, 19)]
+        player_scores = [scores.filter(
+            player=players[x]['player_id']) for x in range(len(players))]
         selected_hole = Hole.objects.filter(id=hole)
-            
+
         context = {
-            'holiday':holiday_filter,
-            'selected_round':selected_round,
-            'tournament':tournament,
-            'scores':scores,
-            "players":players,
+            'holiday': holiday_filter,
+            'selected_round': selected_round,
+            'tournament': tournament,
+            'scores': scores,
+            "players": players,
             "hole_numbers": hole_numbers,
-            "player_scores":player_scores,
-            "hole":selected_hole,
+            "player_scores": player_scores,
+            "hole": selected_hole,
         }
 
-        return render(request,self.template_name,context)
-    
-    
-    
+        return render(request, self.template_name, context)
